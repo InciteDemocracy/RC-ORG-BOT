b85797af15cb67da128054edad7246cd35d44195
Devin <incitedemocracy@gmail.com>
2017-09-07 08:54:45 -1000

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function postNewEvent (source, event) {
       method: 'POST',
       json: true,
       body: {
-        text: `${source}: <https://www.facebook.com/events/${event.id}|${event.owner}> (${event.attending_count} attending, ${event.owner} )`
+        text: `${source}: <https://www.facebook.com/events/${event.id}|${event.owner}> (${event.attending_count} attending, ${JSON.stringify(event.owner)} )`
       }
     });
   }
